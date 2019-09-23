bdccc221e8771c5f4341e3bb906bb0dfa01d6a1d
Darío Peña <leonplondon@gmail.com>
2019-09-23 13:55:39 -0500

    Added test annotations to UserControllerTest class.

--- src/test/java/wolox/training/controllers/UserControllerTest.java
+++ src/test/java/wolox/training/controllers/UserControllerTest.java
@@ -19,9 +19,12 @@ import java.util.List;
 import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import wolox.training.TestUtils;
 import wolox.training.models.Book;
@@ -29,6 +32,8 @@ import wolox.training.models.User;
 import wolox.training.repositories.BookRepository;
 import wolox.training.repositories.UserRepository;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
 
     @Autowired
